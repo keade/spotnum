a67ae55a909eeb2b75b056f2fa4f57bfa4dd17aa
Derek <derek.kean@gmail.com>
2012-06-05 23:21:52 -0500

    reduced sendgrid code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
 
+  def create
+  # Create the user from params
+  @user = User.new(params[:user])
+  if @user.save
+  # Deliver the signup_email
+  Notifier.signup_email(@user).deliver
+  redirect_to root_path, :notice => "User created"
+  else
+  render :action => "new"
+  end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable
+         :recoverable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model :password_confirmation
   attr_accessible :email, :password, :remember_me, :password_confirmation
