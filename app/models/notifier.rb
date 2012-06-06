a922e52cb6ff6b6a94175faa8483a4bb5952aad0
Derek <derek.kean@gmail.com>
2012-06-05 22:27:59 -0500

    fixed ascii

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,4 +1,4 @@
-class Notifier &lt; ActionMailer::Base
+class Notifier < ActionMailer::Base
 default :from => "team@spotnumber.com"
 # send a signup email to the user, pass in the user object that contains the user's email address
 def signup_email(user)
