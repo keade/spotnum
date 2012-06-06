5cba865a3d60d6b0782c43eef3b6a099d932a38f
Derek <derek.kean@gmail.com>
2012-06-05 22:55:26 -0500

    fixed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Finbatsy::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => 'floating-robot-4761.heroku.com' }
   # Enable threaded mode
   # config.threadsafe!
 
