065435d7901b560a8b25297933ef34b71bbc2ccb
Derek <derek.kean@gmail.com>
2012-07-02 19:31:33 -0500

    holy scrappin

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app4969696@heroku.com'],
-  :password       => ENV['yzjbochy'],
-  :domain         => 'heroku.com'
+  :user_name      => 'app4969696@heroku.com',
+  :password       => 'yzjbochy',
+  :domain         => 'spotnumber.com'
 }
 ActionMailer::Base.delivery_method = :smtp
