bacef6fef067198c336b9f8cca2dd7f06079e9a5
Derek Kean <derek.kean@gmail.com>
2015-09-24 10:19:42 +0800

    Update mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,1 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => 'app4969696@heroku.com',
-  :password       => 'yzjbochy',
-  :domain         => 'spotnumber.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+
